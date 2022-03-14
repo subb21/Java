acd93ebfa9482d1209f9cc08cceec0fcf304b9fe
subin.lee <sb020621@naver.com>
2022-03-14 20:46:35 +0900

    [JAVA] hello.java 생성
    hello world를 출력하는 코드 작성

---
+++ src/oop_220310/practice1.java
@@ -0,0 +1,12 @@
+package oop_220310;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("I'm a new Java programmer.");
+
+	}
+
+}
